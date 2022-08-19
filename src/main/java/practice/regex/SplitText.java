6509d5f4138f71395d37947ff395ed7556cbcec2
Anastasya Ivanova <nastya1615325@gmail.com>
2022-08-19 11:06:36 +0400

    Part one- done!

--- src/main/java/practice/regex/SplitText.java
+++ src/main/java/practice/regex/SplitText.java
@@ -3,12 +3,26 @@ package practice.regex;
 public class SplitText {
 
   public static void main(String[] args) {
+    System.out.println(splitTextIntoWords("Ghbdtn!! ghsajdf 555"));
 
   }
 
   public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
+    StringBuilder splitText = new StringBuilder();
+    String regex = "[^A-z ]";
+    text = text.replaceAll(regex,"");
+    String word[] = text.split("\\s+");
+
+    for(int i = 0; i < word.length ;i++){
+      if (i == word.length-1){
+        splitText.append(word[i].replaceAll(regex,""));
+      }
+      else {
+        splitText.append(word[i]).append(System.lineSeparator());
+      }
+
+    }
+    return splitText.toString();
   }
 
 }
