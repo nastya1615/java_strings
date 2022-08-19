8d62410a13d9c02e741a020162674fd7bd2d9f1a
Anastasya Ivanova <nastya1615325@gmail.com>
2022-08-19 09:13:24 +0400

    Task one - done!

--- src/main/java/practice/string/SequentialWordsNumbers.java
+++ src/main/java/practice/string/SequentialWordsNumbers.java
@@ -2,7 +2,44 @@ package practice.string;
 
 public class SequentialWordsNumbers {
 
-    public static String sequentialWordsNumbers(String text){
-        return "";
+
+    public static void main(String[] args) {
+
+        System.out.println(sequentialWordsNumbers(""));
     }
+
+    public static String sequentialWordsNumbers(String text) {
+
+        StringBuilder numerationWords = new StringBuilder("");
+        int index = 1;
+
+
+        for (int i = 0; ; ) {
+            int a = text.indexOf(" ", i);
+            if (a == -1) {
+                String word = text.substring(i);
+
+                if (word.isEmpty()){
+                    numerationWords.append(word);
+                    break;
+                }
+                else {
+                    numerationWords.append("(").append(index).append(") ").append(word);
+                    break;
+                }
+
+            }
+
+            String word = text.substring(i, a);
+            i = a + 1;
+            numerationWords.append("(").append(index).append(") ").append(word).append(" ");
+            index++;
+
+
+
+
+
+    }
+        return numerationWords.toString();
+}
 }
